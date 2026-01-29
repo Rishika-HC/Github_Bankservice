2faa3f63bb75300d327b6ff46356545743dede6c
Rishika-HC <rishikachamaraj123@gmail.com>
2026-01-29 09:47:47 +0530

    Update BankServive.java

--- BankServive.java
+++ BankServive.java
@@ -28,12 +28,12 @@ return balance;
 // The main entry point
 public static void main(String[] args) {
 // Create an instance of the BankAccount class
-BankAccount acc = new BankAccount(5000);
+BankAccount acc = new BankAccount(15000);
 System.out.println("Initial Balance: " + acc.getBalance());
 // Perform operations
-acc.deposit(700);
+acc.deposit(600);
 System.out.println("Balance after deposit of 500: " + acc.getBalance());
-acc.withdraw(100);
+acc.withdraw(500);
 System.out.println("Balance after withdrawal of 300: " + acc.getBalance());
 // Print final result
 System.out.println("Final Balance: " + acc.getBalance());
